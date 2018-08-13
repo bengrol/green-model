37eb5f780f1a0facd255aa7186a9ff6da31b2b49
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-08-13 17:40:46 +0200

    optimized navbar shrink

--- plugins/navbar-shrink.js
+++ plugins/navbar-shrink.js
@@ -1,13 +1,21 @@
+
+import throttle from 'lodash/throttle'
+
+function updateNavbar() {
+  if ($(document).scrollTop() > 50) {
+    // console.log('shrinking navbar...')
+    $('nav').addClass('shrink')
+    $('#logo').attr('src', '/images/Logo_stick.png')
+  } else {
+    $('nav').removeClass('shrink')
+    $('#logo').attr('src', '/images/Logo_main.png')
+  }
+}
+
+var updateNavbarThrottled = throttle(updateNavbar, 1000)
+
 $(document).ready(function() {
 
-  $(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-      $('nav').addClass('shrink')
-      $('#logo').attr('src', 'images/Logo_stick.png')
-    } else {
-      $('nav').removeClass('shrink')
-      $('#logo').attr('src', 'images/Logo_main.png')
-    }
-  })
+  $(window).scroll(updateNavbarThrottled)
 
 })
