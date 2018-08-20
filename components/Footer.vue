18e4ffcf712a4ba3c00cad31e1c45134a901d8dd
B.Groleau <benjamin.groleau@niji.fr>
2018-08-20 17:41:40 +0200

    issues github : adapter model COMPOSANTS About adapter model -TOUS LES COMPOSANTS adapter model COMPOSANTS Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,14 +2,30 @@
   <footer>
     <div class="container">
       <div class="row">
-        <div class="col-md-6 col-md-offset-3">
+        <div class="col-md-4">
           <div class="copyright text-center">
-            <p>
-              <nuxt-link :to="$i18n.path('')">{{ $t('footer.copyright') }}</nuxt-link>
+            <p> <nuxt-link :to="$i18n.path('')">{{ $t('footer.copyright') }}</nuxt-link> </p>
+            <p><a href="http://greenbusinessonly.com" target="_blank" >SITE INTERNATIONAL</a></p>
+            <p><a href="http://www.greenisbetterorder.com/" target="_blank">LIVRAISON A DOMICILE</a></p>
+          </div>
+
+        </div>
+        <div class="col-md-4">
+          <div class="copyright text-center">
+            <p><a href="http://www.greenbusinessonly.com/fr/greenisbetter-pack_expat/" target="_blank">OUVRIR UN GREEN AUX USA</a></p>
+            <p><a href="https://www.slideshare.net/greensaladbars/fr-gib-brochure-jan2017" target="_blank">BROCHURE FRANCHISE</a>
             </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="copyright text-center">
+            <div class="ytb"><a href="https://www.youtube.com/channel/UC79WouLYKZ2q_HKO42bEtcw" target="_blank"><img alt="chaine green is better" src="https://www.greenisbetter.fr/wp-content/uploads/2018/04/yt_logo_rgb_dark.png"> La chaine Green is better</a></div>
           </div>
         </div>
       </div>
     </div>
   </footer>
 </template>
+
+
+
